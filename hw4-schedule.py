16de80c7cb9ef0da7c5555fc1ffe6ef4ba0e918a
Kai-Lin <neversaynever0502@gmail.com>
2020-03-06 01:47:57 +0800

    fix

--- hw4-schedule.py
+++ hw4-schedule.py
@@ -4,11 +4,11 @@ import time
 def job():
   print('早安')
 
-schedule.every().monday.at('07:00').do(job)
-schedule.every().tuesday.at('07:00').do(job)
-schedule.every().wednesday.at('07:00').do(job)
-schedule.every().thursday.at('07:00').do(job)
-schedule.every().friday.at('07:00').do(job)
+schedule.every().monday.at('08:00').do(job)
+schedule.every().tuesday.at('08:00').do(job)
+schedule.every().wednesday.at('08:00').do(job)
+schedule.every().thursday.at('08:00').do(job)
+schedule.every().friday.at('08:00').do(job)
 
 while True:
   schedule.run_pending()
